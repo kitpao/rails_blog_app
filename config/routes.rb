6cb82e1dae9ac32dc75a99fd44655c9ad5a752cc
kitpao <kitpao91@gmail.com>
2020-04-23 15:59:49 -0500

    change the root to articles index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'articles#index'
   resources :articles
 end
