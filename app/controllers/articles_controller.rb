7c67c30081ce2503acd0a82c34b173ab730dd0fb
kitpao <kitpao91@gmail.com>
2020-04-23 15:57:09 -0500

    add more flashes for create and destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,13 +20,13 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+    flash[:eduardo] = "Article '#{@article.id}' Created"
     redirect_to article_path(@article)
   end
 
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
-
     flash[:eduardo] = "Article '#{@article.id}' Updated"
     redirect_to article_path(@article)
   end
@@ -34,6 +34,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+    flash[:eduardo] = "Article '#{@article.id}' No longer exists"
     redirect_to articles_path
   end
 end
