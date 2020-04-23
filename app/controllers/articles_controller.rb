04b68674725be65fbc0cd4a3c87d292ca10a95d0
kitpao <kitpao91@gmail.com>
2020-04-23 15:36:08 -0500

    add edit method with its view and partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
     @article.save
