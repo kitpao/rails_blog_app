a01912b813432a1b8ed0046c7f0d74cdce32c67d
kitpao <kitpao91@gmail.com>
2020-04-23 15:01:05 -0500

    add create method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end
@@ -9,5 +11,11 @@ class ArticlesController < ApplicationController
 
   def new
     @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
   end
 end
