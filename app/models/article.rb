873f28fa256608dc53b8d348711afebfe287cc7c
kitpao <kitpao91@gmail.com>
2020-04-24 16:51:03 -0500

    add logic to filter tags information

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,6 +10,13 @@ class Article < ApplicationRecord
     # end
 
     def tag_list=(tags_string)
+      tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+      #looking in the DB
+      new_or_found_tags = tag_names.collect do |tag_name|
+        Tag.find_or_create_by(name: tag_name)
+      end
+      #attach_to_article
+      self.tags = new_or_found_tags
     end
 
     def tag_list
