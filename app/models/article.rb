577ba2f6d91ae031859844e931021d0ab1160285
Eduardo <jecruxiz@gmail.com>
2020-04-27 16:29:22 -0500

    :sparkles: Introducing attach images and files in the articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ApplicationRecord
   has_many :taggings
   has_many :tags, through: :taggings
   has_attached_file :image
-  validate_attachment_content_type :image, :content_type => ['image/jpg','image/jpeg', 'image/png']
+  validates_attachment_content_type :image, :content_type => ['image/jpg','image/jpeg', 'image/png']
 
   # def tag_list
   #   self.tags.collect do |tag|
