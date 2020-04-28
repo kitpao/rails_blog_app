cfc7c6b672bf806c1a90c1b173700f7526ed1ef8
Eduardo <jecruxiz@gmail.com>
2020-04-28 10:49:22 -0500

    :sparkles: Introducing author session controller
    Adding login and logout actions ,

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
 
   resources :tags
   resources :authors
+  resources :author_sessions, only: [:new, :create, :destroy]
+  
+  #Alias
+  get 'login' => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
 end
