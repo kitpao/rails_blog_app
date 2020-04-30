d11f4b6970e5b77752a83e7d60f85ff67b96cd7d
kitpao <kitpao91@gmail.com>
2020-04-30 12:57:03 -0500

    allow gitpod to act as localhost in navigator

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << "3000-c40f05df-638a-42fe-9c9c-24f42b1a5d8c.ws-us02.gitpod.io"
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
