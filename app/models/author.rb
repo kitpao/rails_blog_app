239a93a13d6a5120d535d38672363f6980f96db4
kitpao <kitpao91@gmail.com>
2020-08-01 23:07:58 +0000

    add validations for authors

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,6 @@
 class Author < ApplicationRecord
   authenticates_with_sorcery!
   validates_confirmation_of :password, message: 'should match confirmation', if: :password
+  validates :username, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
 end
